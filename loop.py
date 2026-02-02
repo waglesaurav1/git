2e92991d74d69d136ad2b5920274ecdad364329f
Saurav Sharma Wagle <saurav34wagle@gmail.com>
2026-02-02 12:55:13 +0545

    Upload

---
+++ loop.py
@@ -0,0 +1,35 @@
+### list in built method
+
+#6. Write a program to add elements to a list using append().
+'''
+a=[1,2,3,4,5]
+a.append(7)
+print(a)
+'''
+# 7.Write a program to insert an element at a specific position in a list.
+'''
+a=[1,2,4,5,6,7]
+a.insert(2,3)
+print(a)
+
+'''
+
+#8.Write a program to remove an element from a list using remove().
+'''
+a=[1,2,3,4,2,5]
+a.remove(2)
+print(a)'''
+
+# 9.Write a program to sort a list in ascending order.
+'''
+a=[2,3,6,1,9,7]
+a.sort()
+print(a)'''
+
+#10.Write a program to count how many times an element appears in a list
+'''
+a=[1,1,2,3,4,1,5,6,7]
+b=a.count(1)
+print(b)
+
+'''
